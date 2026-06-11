21b2994d481352b59ef336f188a6db58ee945bb3
hrh-0 <djhub3rdd543@gmail.com>
2026-06-11 11:20:05 -0300

    Add files via upload

---
+++ Python/TrabajoPractico/Trabajo Practico 1 - Huber Ramírez - 09-06-26/ejercicio2.py
@@ -0,0 +1,18 @@
+#09-06-26
+# Crea un programa que determine si una 
+# persona puede entrar a ver una película de clasificación "Mayores de 18 años".
+
+
+
+
+edad = int(input("Ingrese la edad de Juan: "))
+
+if edad >= 18:
+    input("¡Bienvenido! ¡Puedes pasar!")
+else:
+    input("¡Lo lamento! No tienes la edad suficiente para pasar, necesitas ser mayor a 18 años.")
+
+
+
+
+
